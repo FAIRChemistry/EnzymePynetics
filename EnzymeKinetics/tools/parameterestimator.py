60453647be67fa02bce1b63bf98fd64e4e513038
haeussma <83341109+haeussma@users.noreply.github.com>
2022-11-11 08:58:50 +0100

    added initializer for models

--- EnzymeKinetics/tools/parameterestimator.py
+++ EnzymeKinetics/tools/parameterestimator.py
@@ -1,31 +1,47 @@
+from typing import List, Dict
+
 from EnzymeKinetics.core.enzymekinetics import EnzymeKinetics
 from EnzymeKinetics.core.stoichiometrytypes import StoichiometryTypes
+from kineticmodel import KineticModel, irreversible_model, competitive_product_inhibition_model, uncompetitive_product_inhibition_model, noncompetitive_product_inhibition_model, substrate_inhibition_model, competitive_inhibition_model
 
 import numpy as np
 
 
 class ParameterEstimator():
-    def __init__(self, data: 'EnzymeKinetics'):
+
+    def __init__(self, data: EnzymeKinetics):
         self.data = data
+        self.models: List[KineticModel] = None
         self._initialize_measurement_data()
         self._check_negative_concentrations()
         self.initial_kcat = self._calculate_kcat()
         self.initial_Km = self._calculate_Km()
+        # TODO shapcheck funtion to check for consistent array lengths
 
     def _initialize_measurement_data(self):
+
+        print(type(self.data))
+
 
         measurement_data = []
         initial_substrate = []
         enzyme = []
+        inhibitor = []
 
         for measurement in self.data.measurements:
             for replica in measurement.data:
                 measurement_data.append(replica.values)
                 initial_substrate.append(measurement.initial_substrate_conc)
                 enzyme.append(measurement.enzyme_conc)
+                if measurement.inhibitor_conc != None:
+                    inhibitor.append(measurement.inhibitor_conc)
+                else:
+                    inhibitor.append(0)
 
+        self.time = np.array(self.data.time)
         self.initial_substrate = np.array(initial_substrate)
         self.enzyme = np.array(enzyme)
+        self.inhibitor = np.array(inhibitor)
 
         if self.data.stoichiometry == StoichiometryTypes.SUBSTRATE.value:
             self.substrate = np.array(measurement_data)
@@ -35,6 +51,7 @@ class ParameterEstimator():
             self.substrate = np.array(self._calculate_substrate())
         else:
             raise AttributeError("Please define whether measured data is substrate or product data.")
+
 
     def _calculate_substrate(self):
         substrate = []
@@ -73,29 +90,6 @@ class ParameterEstimator():
             raise ValueError(
                 "Product data contains negative concentrations. Check data.")
 
-    def fit_models(
-        self,
-        initial_substrate_concs: list = None,
-        start_time_index: int = None,
-        stop_time_index: int = None,
-        flag_enzyme_inactivation: bool = False,
-        ):
-
-        # Subset data if any attribute is passed to the function
-        if initial_substrate_concs != None or np.any([start_time_index, stop_time_index]):
-            substrate, product, enzyme, initial_substrate = self._subset_data(
-                initial_substrates=initial_substrate_concs,
-                start_time_index=start_time_index,
-                stop_time_index=stop_time_index)            
-        else:
-            substrate = self.substrate
-            product = self.product
-            enzyme = self.enzyme
-            initial_substrate = self.initial_substrate
-
-        return substrate
-        
-
     def _subset_data(self, initial_substrates: list = None, start_time_index: int = None, stop_time_index: int = None) -> tuple:
         idx = np.array([])
         if initial_substrates == None or len(initial_substrates) == 0:
@@ -113,15 +107,122 @@ class ParameterEstimator():
         new_product = self.product[idx,start_time_index:stop_time_index]
         new_enzyme = self.enzyme[idx]
         new_initial_substrate = self.initial_substrate[idx]
+        new_time = self.time[start_time_index:stop_time_index]
+        new_inhibitor = self.inhibitor[idx]
 
-        return (new_substrate, new_product, new_enzyme, new_initial_substrate)
+        return (new_substrate, new_product, new_enzyme, new_initial_substrate, new_time, new_inhibitor)
+
+
+    def _initialize_models(self, substrate, product, enzyme, inhibitor, enzyme_inactivation: bool) -> Dict[str, KineticModel]:
+
+        if np.all(self.inhibitor == 0):
+
+            irreversible_Michaelis_Menten = KineticModel(
+                name="irreversible Michaelis Menten",
+                params=[],
+                w0={"cS": substrate, "cE": enzyme,
+                    "cP": product, "cI": inhibitor},            
+                kcat_initial=self.initial_kcat,
+                Km_initial=self.initial_Km,
+                model=irreversible_model,
+                enzyme_inactivation=enzyme_inactivation
+            )
+
+            competitive_product_inhibition = KineticModel(
+                name="irreversible Michaelis Menten",
+                params=["K_ic"],
+                w0={"cS": substrate, "cE": enzyme,
+                    "cP": product, "cI": product},            
+                kcat_initial=self.initial_kcat,
+                Km_initial=self.initial_Km,
+                model=competitive_product_inhibition_model,
+                enzyme_inactivation=enzyme_inactivation
+            )
+            uncompetitive_product_inhibition = KineticModel(
+                name="uncompetitive product inhibitioon",
+                params=["K_iu"],
+                w0={"cS": substrate, "cE": enzyme,
+                    "cP": product, "cI": product},            
+                kcat_initial=self.initial_kcat,
+                Km_initial=self.initial_Km,
+                model=uncompetitive_product_inhibition_model,
+                enzyme_inactivation=enzyme_inactivation
+            )
+            noncompetitive_product_inhibition = KineticModel(
+                name="non-competitive product inhibitiion",
+                params=["K_iu", "K_ic"],
+                w0={"cS": substrate, "cE": enzyme,
+                    "cP": product, "cI": product},            
+                kcat_initial=self.initial_kcat,
+                Km_initial=self.initial_Km,
+                model=noncompetitive_product_inhibition_model,
+                enzyme_inactivation=enzyme_inactivation
+            )
+            substrate_inhibition = KineticModel(
+                name="substrate inhibition",
+                params=["K_iu"],
+                w0={"cS": substrate, "cE": enzyme,
+                    "cP": product, "cI": substrate},            
+                kcat_initial=self.initial_kcat,
+                Km_initial=self.initial_Km,
+                model=substrate_inhibition_model,
+                enzyme_inactivation=enzyme_inactivation
+            )
+
+            return {
+                irreversible_Michaelis_Menten.name, irreversible_Michaelis_Menten,
+                competitive_product_inhibition.name, competitive_product_inhibition,
+                uncompetitive_product_inhibition.name, uncompetitive_product_inhibition,
+                noncompetitive_product_inhibition.name, noncompetitive_product_inhibition,
+                substrate_inhibition.name, substrate_inhibition
+            }
+
+               
+        else:
+            print("something went wrong")
+
+    def fit_models(
+        self,
+        initial_substrate_concs: list = None,
+        start_time_index: int = None,
+        stop_time_index: int = None,
+        account_for_enzyme_inactivation: bool = False,
+        ):
+
+        # Subset data if any attribute is passed to the function
+        if initial_substrate_concs != None or np.any([start_time_index, stop_time_index]):
+            substrate, product, enzyme, initial_substrate, time, inhibitor = self._subset_data(
+                initial_substrates=initial_substrate_concs,
+                start_time_index=start_time_index,
+                stop_time_index=stop_time_index)            
+        else:
+            substrate = self.substrate[:,0]
+            product = self.product[:,0]
+            enzyme = self.enzyme
+            initial_substrate = self.initial_substrate
+            time = self.time
+            inhibitor = self.inhibitor
+
+        model_dict = self._initialize_models(
+            substrate=substrate,
+            product=product,
+            enzyme=enzyme,
+            inhibitor=inhibitor,
+            enzyme_inactivation=account_for_enzyme_inactivation
+        )
+
+        ## Models
+
+
+        return model_dict
+        
+
+    
 
 if __name__ == "__main__":
     from EnzymeKinetics.core.measurement import Measurement
     import matplotlib.pyplot as plt
     import numpy as np
-
-
 
     m1 = Measurement(
         initial_substrate_conc=100,
@@ -134,6 +235,7 @@ if __name__ == "__main__":
     m2 = Measurement(
     initial_substrate_conc=200,
     enzyme_conc=0.05,
+    inhibitor_conc=0.0
     )
     m2.add_to_data([1,2,3,4,5,6])
     m2.add_to_data([1.3,2.3,3.3,4.3,5.3,6.3])
@@ -157,10 +259,13 @@ if __name__ == "__main__":
         measurements=[m1,m2,m3],
         time=[0,2,4,6,8,9]
     )
+    print(type(testdata), "YOOOOOO")
 
 
-    test = ParameterEstimator(testdata)
-    print(test.fit_models(initial_substrate_concs=[],start_time_index=4))
+    estimator = ParameterEstimator(data=testdata)
+    print(type(estimator.data))
+    print(estimator.fit_models())
+    #print(test.fit_models(initial_substrate_concs=[],start_time_index=4))
 
 
 
