a3f6c351c04eb89063a10114cb103a0be422e7d6
haeussma <83341109+haeussma@users.noreply.github.com>
2022-11-13 13:06:36 +0100

    deleted

--- EnzymeKinetics/tools/fitter.py
+++
@@ -1,9 +0,0 @@
-from kineticmodel import KineticModel, auto_inhibition_model_dict
-
-class Fitter:
-    def __init__(self, name: str):
-        self.name = name
-
-if __name__ == "__main__":
-    test = Fitter("max")
-    print(test)
