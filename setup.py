881252f69100333b6602b965dddd033c1c4b02b0
haeussma <83341109+haeussma@users.noreply.github.com>
2022-12-15 11:07:27 +0100

    pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ import setuptools
 from setuptools import setup
 
 setup(
-    name="pyyEnzymeKinetics",
+    name="EnzymePynetics",
     version="0.0.1",
     description="Parameter estimation for enzyme kinetics.",
-    url="https://github.com/haeussma/pyyEnzymeKinetics",
+    url="https://github.com/haeussma/EnzymePynetics",
     author="Haeussler, Max",
     author_email="st171427@stud.uni-stuttgart.de",
     packages=setuptools.find_packages(),
     
     install_requires=[
-        "sdRDM @ git+https://github.com/JR-1991/software-driven-rdm.git",
         "ipython==8.6.0",
         "lmfit==1.0.3",
         "matplotlib==3.6.2",
